7ef4ee3577cbc9b2c1492eaf429f3ce7b0a891e0
Raahif Rafeeu <raahif.rafeeu@gmail.com>
2026-04-05 01:35:05 +0500

    Invitation OG description Updated

--- resources/views/invitation.blade.php
+++ resources/views/invitation.blade.php
@@ -5,7 +5,7 @@
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <meta name="csrf-token" content="{{ csrf_token() }}">
 <title>{{ $wedding['groom'] }} & {{ $wedding['bride'] }} — Wedding Invitation</title>
-<meta name="description" content="You're invited to the wedding of {{ $wedding['groom'] }} &amp; {{ $wedding['bride'] }} — {{ $wedding['date'] }}, {{ $wedding['venue'] }}, {{ $wedding['city'] }}.">
+<meta name="description" content="You're invited to the wedding of {{ $wedding['groom'] }} &amp; {{ $wedding['bride'] }}.">
 <meta name="twitter:card" content="summary_large_image">
 <meta property="og:title" content="{{ $wedding['groom'] }} &amp; {{ $wedding['bride'] }} — Wedding Invitation">
 <meta property="og:description" content="You're invited to the wedding of {{ $wedding['groom'] }} &amp; {{ $wedding['bride'] }}.">
